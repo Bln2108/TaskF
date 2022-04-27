1af3f7301eae6feced3b5f9f26eeeb1ccb8f2dd2
Bln2108 <bln2108@yandex.ru>
2022-04-27 16:15:37 +0300

    Новые имена

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int summaNaz = 100; // начальная сумма на счете
-        int summaPop = 1584;  // сумма пополнения
+        int amountInit = 100; // начальная сумма на счете
+        int amountDeposit = 1584;  // сумма пополнения
         int bonus;
-        if (summaPop > 1000) {
-            bonus = summaPop / 100;
+        if (amountDeposit > 1000) {
+            bonus = amountDeposit / 100;
         } else {
             bonus = 0;
         }         // размер бонуса
-        System.out.println("Итоговая сумма на счете равна " + (summaNaz + summaPop + bonus));
+        System.out.println("Итоговая сумма на счете равна " + (amountInit + amountDeposit + bonus));
         System.out.println("Сумма бонусов равна " + bonus);
         // вывод на экран
     }
